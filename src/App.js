321f228ac167f60fcd2ee5bc9096e2732ab6cd86
AgentBond\dbond59 <bonddn@mail.uc.edu>
2018-10-25 13:09:43 -0400

    understand assingment two with the .split method and .join method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 
 class App extends Component {
-  
-  lengthOfTextHandler = (event) => {
-  const textlength = event.target.value;
 
-  return textlength;
+  state = {
+    userInput: ""
+  }
 
-}
+  inputChangedHandler = (event) => {
+    this.setState({ userInput: event.target.value });
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({ userInput: updatedText });
+  }
+
   render() {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />;  
+    });
+
     return (
       <div>
-        <input type='text' onChange={this.lengthOfTextHandler}></input>
-        <p>{this.lengthOfTextHandler.textlength.length}</p>
+        <input
+          type='text'
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput}></input>
+        <br />
+        <hr />
+        <p>{this.state.userInput}</p>
+        <Validation
+          textLength={this.state.userInput.length} />
+        {charList}
       </div>
     )
   }
 }
+
 
 export default App;
